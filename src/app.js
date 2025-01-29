072635f8ce19fb4e2276bf0f640db38ee121be95
Thyrail <atheosc18@gmail.com>
2025-01-29 11:56:19 +0100

    added cors and implemented it into the app.js

--- src/app.js
+++ src/app.js
@@ -1,8 +1,14 @@
 import express from 'express';
 import postRoutes from './routes/post.routes.js';
 import sequelize from './config/database.js';
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use(express.json());
 app.use(postRoutes);
